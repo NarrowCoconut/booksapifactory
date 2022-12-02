ea34a1ce96b5959b2695c8dcd1bb7e5031970c78
McGossy <McGossy@gmail.com>
2022-12-02 17:29:40 -0500

    Rewrote scraper with OOP in mind. Added unit testing for scraper

--- books/services/scraper.py
+++ books/services/scraper.py
@@ -12,6 +12,46 @@ from selenium.webdriver.common.by import By
 # get_book_data_by_xpath(driver, xpath)
 # get_book_data_by_tag_name(driver, tag_name)
 
+class AmazonScrape():
+    def __init__(self, url):
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.get(url)
+        self.scrape_dict = {}
+
+    def get_title(self):
+        title = self.driver.find_element(By.ID, 'productTitle').text
+        self.scrape_dict["title"] = title
+        return(title)
+
+    # working ok 5/10. Need to get it into the read more part though
+    def get_description(self):
+        description = self.driver.find_element(By.ID, 'bookDescription_feature_div').text
+        description = description.replace("\n", '') # we might want new lines later. Depends on how we want to display it on the webpage
+        self.scrape_dict["description"] = description
+        return(description)
+    
+    def get_rating(self):
+        rating = self.driver.find_element(By.CSS_SELECTOR, "span.a-size-medium.a-color-base").text
+        self.scrape_dict['rating'] = rating
+        return(rating)
+
+    def get_rating_pop(self):
+        rating_pop = self.driver.find_element(By.ID, 'acrCustomerReviewText').text
+        self.scrape_dict['rating_pop'] = rating_pop
+        return(rating_pop)
+    
+    def get_image(self):
+        image = self.driver.find_element(By.ID, 'imgBlkFront').get_attribute('src')
+        self.scrape_dict['image'] = image
+        return(image)
+    
+    '''
+    description = driver.find_element(By.ID, 'productDescription').text
+    rating = driver.find_element(By.ID, 'acrCustomerReviewText').text
+    image = driver.find_element(By.ID, 'imgBlkFront').get_attribute('src')
+    '''
 
 def scrape_amazon(url):
     options = webdriver.ChromeOptions()
@@ -19,11 +59,19 @@ def scrape_amazon(url):
     driver = webdriver.Chrome(options=options)
     driver.get(url)
     title = driver.find_element(By.ID, 'productTitle').text
-    # description = driver.find_element(By.ID, 'productDescription').text
-    # rating = driver.find_element(By.ID, 'acrCustomerReviewText').text
-    # image = driver.find_element(By.ID, 'imgBlkFront').get_attribute('src')
+    #description = driver.find_element(By.ID, 'productDescription').text
+    #rating = driver.find_element(By.ID, 'acrCustomerReviewText').text
+    #image = driver.find_element(By.ID, 'imgBlkFront').get_attribute('src')
     driver.quit()
     return {'title': title, 
     # 'description': description, 'rating': rating, 'image': image, 
     'url': url
     }
+
+scraper = AmazonScrape("https://www.amazon.com/Python-Crash-Course-Hands-Project-Based/dp/1593279280/ref=sr_1_1?dchild=1&keywords=python+crash+course&qid=1605078487&sr=8-1")
+print("\n\n", scraper.get_title())
+print(scraper.get_description())
+print(scraper.get_rating())
+print(scraper.get_rating_pop())
+print(scraper.get_image())
+print(scraper.scrape_dict)
--- tests/unit/scraper_test.py
+++ tests/unit/scraper_test.py
@@ -2,12 +2,74 @@ import pytest
 
 #  tests the scraper.py file
 from books.services.scraper import scrape_amazon #  tests the scrape_amazon function
+from books.services.scraper import AmazonScrape # tests the AmazonScraper class
 # TODO: separate into multiple tests
 def test_scrape_amazon_title():
     #  test the scraper with a valid url
     url = 'https://www.amazon.com/Python-Crash-Course-Hands-Project-Based/dp/1593279280/ref=sr_1_1?dchild=1&keywords=python+crash+course&qid=1605078487&sr=8-1'
     result = scrape_amazon(url)
     assert result['title'] == 'Python Crash Course, 2nd Edition: A Hands-On, Project-Based Introduction to Programming'
+
+
+
+py_crash_url = "https://www.amazon.com/Python-Crash-Course-Hands-Project-Based/dp/1593279280/ref=sr_1_1?dchild=1&keywords=python+crash+course&qid=1605078487&sr=8-1"
+scraper = AmazonScrape(py_crash_url)
+
+def test_scrape_title():
+    result = scraper.get_title()
+    assert result == "Python Crash Course, 2nd Edition: A Hands-On, Project-Based Introduction to Programming"
+
+def test_scrape_description():
+    result = scraper.get_description()
+    assert result == '''If you've been thinking about learning how to code or picking up Python, this internationally bestselling guide to the most popular programming language is your quickest, easiest way to get started and go! Even if you have no experience whatsoever, Python Crash Course, 2nd Edition, will have you writing programs, solving problems, building computer games, and creating data visualizations in no time.
+
+                        You’ll begin with basic concepts like variables, lists, classes, and loops—with the help of fun skill-strengthening exercises for every topic—then move on to making interactive programs and best practices for testing your code. Later chapters put your new knowledge into play with three cool projects: a 2D Space Invaders-style arcade game, a set of responsive data visualizations you’ll build with Python's handy libraries (Pygame, Matplotlib, Plotly, Django), and a customized web app you can deploy online.
+
+                         Why wait any longer? Start your engine and code!'''
+
+def test_scrape_rating():
+    result = scraper.get_rating()
+    assert "out of 5" in result
+
+def test_scrape_rating_pop():
+    result = scraper.get_rating_pop()
+    assert "ratings" in result
+
+def test_scrape_image():
+    result = scraper.get_image()
+    assert result == "https://m.media-amazon.com/images/I/51OOCVBfCQL._SX377_BO1,204,203,200_.jpg"
+
+
+abs_url = "https://www.amazon.com/Automate-Boring-Stuff-Python-2nd/dp/1593279922/ref=pd_bxgy_img_sccl_2/134-3464273-1606430?pd_rd_w=FtGyW&content-id=amzn1.sym.7f0cf323-50c6-49e3-b3f9-63546bb79c92&pf_rd_p=7f0cf323-50c6-49e3-b3f9-63546bb79c92&pf_rd_r=18HCR33KA6JMNTHCVY44&pd_rd_wg=CO0gU&pd_rd_r=edebd344-02f6-4085-92bf-0667eaa7c005&pd_rd_i=1593279922&psc=1"
+abs_scraper = AmazonScrape(abs_url)
+
+def test_abs_scrape_title():
+    result = abs_scraper.get_title()
+    assert result == "Automate the Boring Stuff with Python, 2nd Edition: Practical Programming for Total Beginners"
+
+def test_abs_scrape_description():
+    result = abs_scraper.get_description()
+    assert result == '''Learn how to code while you write programs that effortlessly perform useful feats of automation!
+
+                        The second edition of this international fan favorite includes a brand-new chapter on input validation, Gmail and Google Sheets automations, tips for updating CSV files, and more.
+
+                        If you've ever spent hours renaming files or updating spreadsheet cells, you know how tedious tasks like these can be. But what if you could have your computer do them for you? Automate the Boring Stuff with Python, 2nd Edition teaches even the technically uninclined how to write programs that do in minutes what would take hours to do by hand—no prior coding experience required!
+
+                        This new, fully revised edition of Al Sweigart’s bestselling Pythonic classic, Automate the Boring Stuff with Python, covers all the basics of Python 3 while exploring its rich library of modules for performing specific tasks, like scraping data off the Web, filling out forms, renaming files, organizing folders, sending email responses, and merging, splitting, or encrypting PDFs. There’s also a brand-new chapter on input validation, tutorials on automating Gmail and Google Sheets, tips on automatically updating CSV files, and other recent feats of automations that improve your efficiency.
+
+                        Detailed, step-by-step instructions walk you through each program, allowing you to create useful tools as you build out your programming skills, and updated practice projects at the end of each chapter challenge you to improve those programs and use your newfound skills to automate similar tasks. Boring tasks no longer have to take to get through—and neither does learning Python!'''
+
+def test_abs_scrape_rating():
+    result = abs_scraper.get_rating()
+    assert "out of 5" in result
+
+def test_abs_scrape_rating_pop():
+    result = abs_scraper.get_rating_pop()
+    assert "ratings" in result
+
+def test_abs_scrape_image():
+    result = abs_scraper.get_image()
+    assert result == "https://m.media-amazon.com/images/I/51B161E04DL._SX375_BO1,204,203,200_.jpg"
 
 # #  tests the scrape_amazon function
 # def test_scrape_amazon():
